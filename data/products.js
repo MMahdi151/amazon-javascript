faa226194aa3a3ddf9baa6c8402860115600a616
Mahdi <mmahdikarimi2006@gmail.com>
2024-07-03 14:09:35 +0430

    create appliances class

--- data/products.js
+++ data/products.js
@@ -53,6 +53,23 @@ class Clothing extends Product {
   }
 }
 
+class Appliances extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.WarrantyLink = productDetails.WarrantyLink;
+  }
+  extraInfoHTML() {
+    return `<a href="images/appliance-instructions.png"
+    target="_blank">Instructions</a></br>
+    <a href="images/appliance-warranty.png"
+    target="_blank">Warranty</a>`;
+  }
+}
+
 // const date = new Date();
 // console.log(date);
 // console.log(date.toLocaleTimeString());
@@ -119,6 +136,9 @@ export const products = [
     },
     priceCents: 1899,
     keywords: ["toaster", "kitchen", "appliances"],
+    type: "appliances",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png",
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -253,6 +273,9 @@ export const products = [
     },
     priceCents: 3074,
     keywords: ["water boiler", "appliances", "kitchen"],
+    type: "appliances",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png",
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -466,6 +489,9 @@ export const products = [
     },
     priceCents: 2250,
     keywords: ["coffeemakers", "kitchen", "appliances"],
+    type: "appliances",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png",
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -510,6 +536,9 @@ export const products = [
     },
     priceCents: 10747,
     keywords: ["food blenders", "kitchen", "appliances"],
+    type: "appliances",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png",
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -569,6 +598,8 @@ export const products = [
 ].map((productDetails) => {
   if (productDetails.type === "clothing") {
     return new Clothing(productDetails);
+  } else if (productDetails.type === "appliances") {
+    return new Appliances(productDetails);
   }
   return new Product(productDetails);
 });
